95b06b0cbb4932033ec7344dab4e7f7fa779dcd2
Dimadzu <Dimadzu@i.ua>
2021-05-29 21:26:13 +0300

    fix: function slider on page zoos

--- online-zoo/js/zoos.js
+++ online-zoo/js/zoos.js
@@ -19,4 +19,47 @@ let vidFrame=document.querySelector(".video"),
     })
 }
 choiseVideo();
-})
+const gap = 40;
+
+let carousel = document.querySelector('.hidden_cont_othercams'),
+  content = document.querySelector('.grid_cams'),
+  nextCams = document.querySelector('.btn_right_cams '),
+  prevCams = document.querySelector('.btn_left_cams'),
+  sliderItemArr=document.querySelectorAll('.grid_cams .item'),
+  contentWidth= content.offsetWidth,
+  carouselWidth=carousel.offsetWidth,
+  posRightXcarousel=carousel.offsetLeft+carouselWidth,
+  posCarousel=carousel.getBoundingClientRect(),
+  posContent=content.getBoundingClientRect(),
+  arrItem=[];
+  arrItem.push(sliderItemArr);
+//   document.addEventListener('mousemove',(e)=>{
+//       let x=e.pageX;
+//       console.log(x)
+//   })
+
+let imgWidth = document.querySelector('.grid_cams .item').offsetWidth;
+window.addEventListener('resize', (e) => {
+    carouselWidth = carousel.offsetWidth;
+    imgWidth = imgWidth.offsetWidth;
+});
+
+nextCams.addEventListener('click', e => {
+    console.log(imgWidth)
+    carousel.scrollTo((imgWidth + gap), 0);
+ for(let i=0;i<sliderItemArr.length;i++){
+    posItemSlider=sliderItemArr[sliderItemArr.length-1].getBoundingClientRect();
+
+    }
+    if (posItemSlider.right<=posCarousel.right){
+        console.log("yed")
+    };  
+});
+
+prevCams.addEventListener('click', e => {
+  carousel.scrollTo(-(imgWidth + gap), 0);
+});
+
+
+});
+
